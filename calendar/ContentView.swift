b8d40f46509734c3a9e0cc3ffef01c9d79a605f6
Leavelefe <45840530+Leavelefe@users.noreply.github.com>
2023-02-06 09:55:19 +0800

    Initial Commit

---
+++ calendar/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  calendar
+//
+//  Created by 王峥 on 2023/2/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ calendar/calendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  calendarApp.swift
+//  calendar
+//
+//  Created by 王峥 on 2023/2/6.
+//
+
+import SwiftUI
+
+@main
+struct calendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
